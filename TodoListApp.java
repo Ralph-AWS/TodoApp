9610dbac8716de72c543281b1e3c7d7248538310
ralph estonio <ralph.estonio@awsys-i.com>
2025-04-21 14:51:35 +0800

    Add Task class and basic functionality

--- TodoListApp.java
+++ TodoListApp.java
@@ -1,3 +1,25 @@
-public class TodoListApp {
 
+import java.util.ArrayList;
+
+public class TodoListApp {
+    static class Task {
+        String description;
+        boolean isCompleted;
+
+        Task(String description) {
+            this.description = description;
+            this.isCompleted = false;
+        }
+
+        @Override
+        public String toString() {
+            return (isCompleted ? "[✔]" : "[ ]") + " " + description;
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Task> tasks = new ArrayList<>();
+        System.out.println("Todo List App Initialized!");
+    }
 }
+
